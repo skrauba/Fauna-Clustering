ef8bdbf99bc5381179a1fe895cbf11b47f7177bc
AxelSkrauba <axelskrauba@gmail.com>
2024-10-15 10:57:05 -0300

    Added files according to basic project structure.

---
+++ app/processing/image_processing.py
@@ -0,0 +1,10 @@
+import numpy as np
+from PIL import Image
+
+def process_image(image_path):
+    # Implementar la lógica de procesamiento de imágenes
+    pass
+
+def detect_fauna(processed_image):
+    # Implementar la lógica de detección de fauna
+    pass
