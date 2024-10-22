64f299243954f349dabd80bda8229431925b03bc
Keller-Javi <kellerjavier13@gmail.com>
2024-10-22 15:55:17 -0300

    Added Drag&Drop Folder

--- app/logic/selection_screen.py
+++ app/logic/selection_screen.py
@@ -1,9 +1,18 @@
+from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen
 from kivy.properties import StringProperty
 
 class SelectionScreen(Screen):
-    selected_folder = StringProperty('')
+    selected_folder = StringProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Window.bind(on_dropfile=self.on_folder_drop)  # Enlazar el evento de arrastrar carpeta
+
+    def on_folder_drop(self, window, file_path):
+        self.selected_folder = str(file_path.decode('utf-8'))
+        print('La carpeta seleccionada es: ', self.selected_folder)
 
     def select_folder(self):
-        # Implementar la lógica para seleccionar una carpeta
-        pass
+        # Lógica para abrir un selector de archivos o manejar la selección de carpeta
+        print('Selección de carpeta activada.')
